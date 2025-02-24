37c58564d863e5daf7de89b2bbd9fadf8e490f8f
SArcD <141703135+SArcD@users.noreply.github.com>
2025-02-23 22:36:23 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1705,22 +1705,22 @@ elif paginas == "Análisis":
     import matplotlib.pyplot as plt
 
     # Escalas Likert
-    escala_likert_positiva = {"Siempre": 4, "Casi siempre": 3, "Algunas Veces": 2, "Casi nunca": 1, "Nunca": 0}
-    escala_likert_negativa = {"Siempre": 0, "Casi siempre": 1, "Algunas Veces": 2, "Casi nunca": 3, "Nunca": 4}
+    #escala_likert_positiva = {"Siempre": 4, "Casi siempre": 3, "Algunas Veces": 2, "Casi nunca": 1, "Nunca": 0}
+    #escala_likert_negativa = {"Siempre": 0, "Casi siempre": 1, "Algunas Veces": 2, "Casi nunca": 3, "Nunca": 4}
 
     # Diccionario de dominios
-    dominios_reales = {
-        "Condiciones en el ambiente de trabajo": ["P2_1", "P2_2", "P2_3", "P2_4", "P2_5"],
-        "Carga de trabajo": ["P3_1", "P3_2", "P3_3", "P4_1", "P4_2", "P4_3", "P4_4", "P15_1", "P15_2", "P15_3", "P15_4", "P5_1", "P5_2", "P5_3", "P5_4"],
-        "Falta de control sobre el trabajo": ["P7_1", "P7_2", "P7_3", "P7_4", "P7_5", "P7_6", "P8_1", "P8_2", "P9_5", "P9_6"],
-        "Jornada de trabajo": ["P6_1", "P6_2"],
-        "Interferencia en la relación trabajo-familia": ["P6_3", "P6_4", "P6_5", "P6_6"],
-        "Liderazgo": ["P9_1", "P9_2", "P9_3", "P9_4", "P10_1", "P10_2", "P10_3", "P10_4", "P10_5"],
-        "Relaciones en el trabajo": ["P11_1", "P11_2", "P11_3", "P11_4", "P11_5", "P17_1", "P17_2", "P17_3", "P17_4"],
-        "Violencia": ["P13_1", "P13_2", "P13_3", "P13_4", "P13_5", "P13_6", "P13_7", "P13_8"],
-        "Reconocimiento del desempeño": ["P12_1", "P12_2", "P12_3", "P12_4", "P12_5", "P12_6"],
-        "Insuficiente sentido de pertenencia e inestabilidad": ["P12_7", "P12_9", "P12_10", "P12_8"]
-    }
+    #dominios_reales = {
+    #    "Condiciones en el ambiente de trabajo": ["P2_1", "P2_2", "P2_3", "P2_4", "P2_5"],
+    #    "Carga de trabajo": ["P3_1", "P3_2", "P3_3", "P4_1", "P4_2", "P4_3", "P4_4", "P15_1", "P15_2", "P15_3", "P15_4", "P5_1", "P5_2", "P5_3", "P5_4"],
+    #    "Falta de control sobre el trabajo": ["P7_1", "P7_2", "P7_3", "P7_4", "P7_5", "P7_6", "P8_1", "P8_2", "P9_5", "P9_6"],
+    #    "Jornada de trabajo": ["P6_1", "P6_2"],
+    #    "Interferencia en la relación trabajo-familia": ["P6_3", "P6_4", "P6_5", "P6_6"],
+    #    "Liderazgo": ["P9_1", "P9_2", "P9_3", "P9_4", "P10_1", "P10_2", "P10_3", "P10_4", "P10_5"],
+    #    "Relaciones en el trabajo": ["P11_1", "P11_2", "P11_3", "P11_4", "P11_5", "P17_1", "P17_2", "P17_3", "P17_4"],
+    #    "Violencia": ["P13_1", "P13_2", "P13_3", "P13_4", "P13_5", "P13_6", "P13_7", "P13_8"],
+    #    "Reconocimiento del desempeño": ["P12_1", "P12_2", "P12_3", "P12_4", "P12_5", "P12_6"],
+    #    "Insuficiente sentido de pertenencia e inestabilidad": ["P12_7", "P12_9", "P12_10", "P12_8"]
+    #}
 
     st.title("Árboles de Decisión para Predecir el Nivel de Riesgo por Dominio")
 
